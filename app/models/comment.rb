02595159db235387e8fddd1b5e9c0a5e3d90cab2
Aaliyah Harper <aeverly14@pm.me>
2025-06-13 12:55:27 -0400

    add rich text support

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+  has_rich_text :body
   belongs_to :post
   belongs_to :user
   validates :body, presence: true, length: { minimum: 10, maximum: 500 }
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
+  has_rich_text :content
   belongs_to :user
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
-  validates :body, presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :content, presence: true, length: { minimum: 10, maximum: 1000 }
 end
