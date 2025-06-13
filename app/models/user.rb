d10cb40044233c002b59f0306f34b13c082ff82f
Aaliyah Harper <aeverly14@pm.me>
2025-06-13 12:51:49 -0400

    generate comments model & migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
 end
